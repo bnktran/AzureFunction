81745f17562520726e0a70e7eb9dbf28ff60e960
Bao Ngoc Tran <tranbn1@gmail.com>
2020-04-10 21:40:28 -0500

    Add Unit Testing

---
+++ WatchFunctionsTests/WatchFunctionsTests.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace WatchFunctionsTests
+{
+    public class WatchFunctionsTests
+    {
+        [Fact]
+        public void TestWatchFunctionSuccess()
+        {
+            var httpContext = new DefaultHttpContext();
+            var queryStringValue = "abc";
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection
+                (
+                    new System.Collections.Generic.Dictionary<string, StringValues>()
+                    {
+                        { "model", queryStringValue }
+                    }
+                )
+            };
+
+            // Create a dummy logger
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            // Invoke WatchInfo function, with request and logger as parameters
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "OK" response
+            Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (OkObjectResult)response.Result;
+            dynamic watchinfo = new { Manufacturer = "Abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
+            string watchInfo = $"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}";
+            Assert.Equal(watchInfo, result.Value);
+        }
+
+        // Test function fails if isn't given a query string
+        [Fact]
+        public void TestWatchFunctionFailureNoQueryString()
+        {
+            var httpContext = new DefaultHttpContext();
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "Bad" response
+            Assert.IsAssignableFrom<BadRequestObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (BadRequestObjectResult)response.Result;
+            Assert.Equal("Please provide a watch model in the query string", result.Value);
+        }
+
+        // Test function is passed a query string that doesn't contain model parameter
+        [Fact]
+        public void TestWatchFunctionFailureNoModel()
+        {
+            var httpContext = new DefaultHttpContext();
+            var queryStringValue = "abc";
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection
+                (
+                    new System.Collections.Generic.Dictionary<string, StringValues>()
+                    {
+                        { "not-model", queryStringValue }
+                    }
+                )
+            };
+
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var response = WatchPortalFunction.WatchInfo.Run(request, logger);
+            response.Wait();
+
+            // Check that the response is an "Bad" response
+            Assert.IsAssignableFrom<BadRequestObjectResult>(response.Result);
+
+            // Check that the contents of the response are the expected contents
+            var result = (BadRequestObjectResult)response.Result;
+            Assert.Equal("Please provide a watch model in the query string", result.Value);
+        }
+    }
+}
